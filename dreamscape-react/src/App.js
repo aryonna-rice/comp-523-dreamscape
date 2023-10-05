06b8edb63623f556aaf66ac88650b71b38853c33
Karen Gonzalez-Palomo <karengp8@ad.unc.edu>
2023-10-05 14:40:40 -0400

    react setup

---
+++ dreamscape-react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import * as React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Welcome to Dreamscape!
+        </h1>
+        <p>
+          We're currently building this site.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
