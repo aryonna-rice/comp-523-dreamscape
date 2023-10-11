6ba0fc1912f9c1c1cf69b06826de965b06be5689
Karen Gonzalez-Palomo <karengp8@ad.unc.edu>
2023-10-10 20:52:52 -0400

    header created

--- dreamscape-react-native/App.js
+++ dreamscape-react-native/App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header as HeaderRNE, HeaderProps, Icon } from '@rneui/themed';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {Dimensions} from 'react-native';
+import { Image } from '@rneui/themed';
+
+const logo = require('./assets/logo.png')
+const windowWidth = Dimensions.get('window').width;
+const screenWidth = Dimensions.get('screen').width;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Dreamscape!</Text>
-      <Text>We're currently building this site.</Text>
+      <SafeAreaProvider>
+      <HeaderRNE
+            leftComponent={
+              <View style={styles.headerLeft}>
+                <Image
+                source={logo}
+                containerStyle={styles.logo}
+                />
+                <Text style={styles.header1}>DreamScape <Text style={styles.header2}>Admin</Text></Text>
+              </View>
+            }
+            rightComponent={
+              <View style={styles.headerRight}>
+                <Text style={styles.paragraph}>Home</Text> 
+                <Text style={styles.paragraph}>All data</Text>
+                <Text style={styles.paragraph}>Find User</Text>
+              </View>
+          }
+            containerStyle={styles.headerContainer}
+      />
+      </SafeAreaProvider>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +41,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#19173D',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerContainer: {
+    backgroundColor: '#19173D',
+    width: windowWidth,
+    flexDirection: 'row',
+    border: 'none',
+  },
+  headerLeft: {
+    flexDirection: 'row',
+    width: windowWidth/2,
+  },
+  headerRight: {
+    flexDirection: 'row',
+  },
+  header1: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  header2: {
+    color: '#5A6BFF',
+    fontSize: 24,
+  },
+  paragraph: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    padding: 10,
+  },
+  logo: {
+    width: 100,
+    height: 100,
   },
 });
