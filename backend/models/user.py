d225ac6bc2a51970f5f0d8bbc2583ae4b40270f4
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-10-16 14:57:24 -0400

    Create a user model based on client wants
    Created a model that is intended to take a first and last name from the user and some sort of a device identifier automatically, with the idea being that we would search the database for this identifier then automatically "log-in" the user if it exists.

---
+++ backend/models/user.py
@@ -0,0 +1,10 @@
+"""A data model for a registered user."""
+
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int | None
+    first_name: str
+    last_name: str
+    # This would be how we associate a device with a name and id, not sure what the data type will be here
+    device_id: int
