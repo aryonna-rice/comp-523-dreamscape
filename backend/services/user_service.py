ce40dce556bdbdd71be0cf71ba7330bcf189bb39
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-10-27 21:45:56 -0400

    Add service functions for listing and searching users

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -13,6 +13,7 @@ class UserService:
         self._session = session
 
     def get(self, device_id: int) -> User | None:
+        '''Get a user via device id, all users'''
         query = select(UserEntity).where(UserEntity.device_id == device_id)
         user_entity: UserEntity = self._session.scalar(query)
         if user_entity is None:
@@ -20,7 +21,23 @@ class UserService:
         else:
             model = user_entity.to_model()
             return model
+
+    def search_name(self, first_name: str | None, last_name: str | None) -> list[User]:
+        '''Get a list of users via first and/or last name search, admin users'''
+        query = select(UserEntity)
+        if (first_name):
+            query = query.where(UserEntity.first_name == first_name)
+        if (last_name):
+            query = query.where(UserEntity.last_name == last_name)
+        user_entities = self._session.execute(query).scalars()
+        return [entity.to_model() for entity in user_entities]
         
+    def list_users(self) -> list[User]:
+        '''Get a list of users, admin users'''
+        query = select(UserEntity)
+        user_entities = self._session.execute(query).scalars()
+        return [entity.to_model() for entity in user_entities]
+
     def add(self, user: User) -> User | None:
         '''Function used to add to the database'''
         entity = UserEntity.from_model(user)
