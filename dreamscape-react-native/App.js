8076d9e6b6387b74ed561d712dccbd19072c2361
Karen Gonzalez-Palomo <karengp8@ad.unc.edu>
2023-11-14 14:36:09 -0500

    search by last name

--- dreamscape-react-native/App.js
+++ dreamscape-react-native/App.js
@@ -21,13 +21,20 @@ export default function App() {
   const [open, setOpen] = useState(false)
 
   const [deviceId, setDeviceId] = useState('');
+  const[searchLast, setSearchLast] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
-
   const handleSearchSubmit = () => {
-    console.log('Searching for patient with Device ID:', deviceId);
+    let apiUrl = '';
+    if (deviceId) {
+      apiUrl = `http://localhost:8000/api/user?device_id=${deviceId}`;
+    } else if (searchLast) {
+      apiUrl = `http://localhost:8000/api/user/search?last_name=${searchLast}`;
+    }
+
+    console.log('Searching for patient with:', deviceId ? 'Device ID ' + deviceId : 'Last Name ' + searchLast);
     axios
-      .get(`http://localhost:8000/api/user?device_id=${deviceId}`)
+      .get(apiUrl)
       .then((response) => {
         // Handle success (e.g., display search results)
         if (response.data === null || response.data.length === 0) {
@@ -35,7 +42,13 @@ export default function App() {
           Alert.alert('No Results', 'No matching results found.');
         } else {
           // Update search results state
-          setSearchResults(response.data);
+          console.log('Search results:', response.data);
+          if (deviceId) {
+            setSearchResults(response.data);
+          }
+          else if (searchLast) {
+            setSearchResults(response.data[0]);
+          }
         }
         // You can handle the search results as needed for your application
       })
@@ -179,11 +192,29 @@ export default function App() {
           style={styles.input}  
           placeholder="Device ID"
           value={deviceId}
-          onChangeText={text => setDeviceId(text)}
+          onChangeText={text => {
+            // Allow editing only if the Device ID field is empty
+            if (!searchLast) {
+              setDeviceId(text);
+            }
+          }}
+        />
+        <Text style={styles.header2}>OR</Text>
+        <Input
+          style={styles.input}
+          placeholder="Last Name"
+          value={searchLast}
+          onChangeText={text => {
+            // Allow editing only if the Device ID field is empty
+            if (!deviceId) {
+              setSearchLast(text);
+            }
+          }}
         />
         <Button type="submit" style={styles.search} onPress={handleSearchSubmit} ><SearchOutlined /></Button>
+        {/* <Text style={styles.paragraph}>First Name: {searchResults.first_name}</Text> */}
         <FlatList
-          data={[searchResults.first_name, searchResults.last_name, searchResults.dob, searchResults.gender]}
+          data={[searchResults.id, searchResults.first_name, searchResults.last_name, searchResults.dob, searchResults.gender]}
           renderItem={({ item }) => ( 
             <Text style={styles.paragraph}>
               {item}
