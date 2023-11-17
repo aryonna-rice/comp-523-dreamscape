14a91a8574a1c932442a7950e22e5fcc0f73cdb1
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-11-17 16:45:41 -0500

    Update create_database to create tables
    This was an oversight on my part

--- backend/script/create_database.py
+++ backend/script/create_database.py
@@ -1,6 +1,9 @@
 '''Script for creating a database during development'''
 
 import sqlalchemy
+from .. import entities
 
 # Use a database file for testing
 engine = sqlalchemy.create_engine("sqlite+pysqlite:///database.db", echo=True)
+
+entities.EntityBase.metadata.create_all(engine)
