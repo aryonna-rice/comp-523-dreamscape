0487cb6e73e17215d2042dd8b4394100c0a52979
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-11-30 21:18:24 -0500

    Connect database to gcp instance
    Create a function that pulls data for the database from a .env file that is now required.

--- backend/script/create_database.py
+++ backend/script/create_database.py
@@ -3,7 +3,7 @@
 import sqlalchemy
 from .. import entities
 
-# Use a database file for testing
+# Use a database file for local testing, change the string in database.py to this to redirect to local
 engine = sqlalchemy.create_engine("sqlite+pysqlite:///database.db", echo=True)
 
 entities.EntityBase.metadata.create_all(engine)
